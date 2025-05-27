3cc8ed82f31e48c4efd4d317a9eb1870be1f36b1
AMIRHOSEIN HASANLI <38242249+AmirSailor@users.noreply.github.com>
2025-05-27 18:41:44 +0200

    Update link_finder.py

--- link_finder.py
+++ link_finder.py
@@ -2,14 +2,15 @@ from html.parser import HTMLParser
 from urllib import parse
 
 
-class linkfinder(HTMLParser):
+class LinkFinder(HTMLParser):
 
-    def __init__(self, base_url, page_url, *, convert_charrefs = True):
-        super().__init__(convert_charrefs=convert_charrefs)
-        self.base_url= base_url
+    def __init__(self, base_url, page_url):
+        super().__init__()
+        self.base_url = base_url
         self.page_url = page_url
         self.links = set()
 
+    # When we call HTMLParser feed() this function is called when it encounters an opening tag <a>
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             for (attribute, value) in attrs:
@@ -20,6 +21,5 @@ class linkfinder(HTMLParser):
     def page_links(self):
         return self.links
 
-
-    def error(self, massage):
+    def error(self, message):
         pass
