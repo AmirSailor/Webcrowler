ab35cdfa3b4bdb21342c84c1ff1fa9f7720ff45d
AMIRHOSEIN HASANLI <38242249+AmirSailor@users.noreply.github.com>
2025-05-27 18:47:18 +0200

    Update domain.py
    commenting

--- domain.py
+++ domain.py
@@ -1,14 +1,17 @@
 from urllib.parse import urlparse
 
-def get_domain(url):
+
+# Get domain name (example.com)
+def get_domain_name(url):
     try:
-        results = get_sub_domains(url).split('.')
+        results = get_sub_domain_name(url).split('.')
         return results[-2] + '.' + results[-1]
     except:
         return ''
 
-#get sub domains (main.example.com)
-def get_sub_domains(url):
+
+# Get sub domain name (name.example.com)
+def get_sub_domain_name(url):
     try:
         return urlparse(url).netloc
     except:
