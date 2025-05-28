decd7d79622f175a2fc684036ecd4f615a0bfb15
AMIRHOSEIN HASANLI <38242249+AmirSailor@users.noreply.github.com>
2025-05-28 19:12:26 +0200

    #update
    now u can exclude tags or classes for more data clearnes

---
+++ exclude.py
@@ -0,0 +1,10 @@
+EXCLUDE_TAGS = ['nav', 'script', 'style', 'header', 'footer', 'figure']
+EXCLUDE_CLASSES = ['header__inner', 'social-share__title',
+                    'social-share__items', 'content-group__title',
+                      'content-group__body has-columns-4', 'footer f156hidz',
+                        'footer__column footer__column--5', 'footer__column-content',
+                          'logo logo--full footer__logo l979h8x', 'footer__details',
+                            'footer__address', 'footer__copyright', 'footer__social',
+                              'footer__social-link', 'footer__social-icon',
+                                'footer__social-text', 'simple-menu__link', 'simple-menu__item',
+                                'list simple-menu__list']
